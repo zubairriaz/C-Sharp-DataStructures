8c887fad452bb17240c60968a65ffbde040431cf
Zubair <zubair.riaz@trapezegroup.com>
2021-03-12 12:24:54 +0500

    Initial Commit

---
+++ Collections/ConsoleApp1/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Movie
+    {
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ Collections/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie> {
+               new Movie { Name="ABC", Rating=8,Year=1999} ,
+               new Movie { Name = "DEF", Rating = 8, Year = 1999 } ,
+               new Movie { Name = "GHI", Rating = 8, Year = 1999 },
+               new Movie { Name = "JKL", Rating = 8, Year = 1999 } };
+
+            var filteredMovies = movies.FindAll(x => x.Rating > 7);
+            foreach(var movie in filteredMovies)
+            {
+                Console.WriteLine(movie.Name);
+            }
+        }
+    }
+}
