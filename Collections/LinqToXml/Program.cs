737aff5172a7dea074e00f726db63d0df963f6ab
unknown <zubairriaz78696@gmail.com>
2021-03-15 12:23:00 -0700

    entity framework

--- Collections/LinqToXml/Program.cs
+++ Collections/LinqToXml/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace LinqToXml
 {
@@ -6,7 +8,15 @@ namespace LinqToXml
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var document = XDocument.Load("sample.xml");
+            var xmlCollection = document.Element("catalog").Elements("book");
+            var nodes = xmlCollection.Where(x => x.Attribute("id").Value == "bk101");
+            
+
+            foreach(var res in nodes)
+            {
+                Console.WriteLine($"{res.Element("author").Value}");
+            }
         }
     }
 }
