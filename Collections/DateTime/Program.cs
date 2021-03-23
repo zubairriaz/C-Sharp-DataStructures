662aaa31c03c5f4084a45897b0a2f90624ffda1d
unknown <zubairriaz78696@gmail.com>
2021-03-23 15:45:52 +0500

    Update Program.cs
    updates to c# learning

--- Collections/DateTime/Program.cs
+++ Collections/DateTime/Program.cs
@@ -7,6 +7,36 @@ namespace DateTime
     {
         static void Main(string[] args)
         {
+            CommonDateTimeScenarios();
+        }
+
+
+        static void CommonDateTimeScenarios()
+        {
+
+        }
+
+        static void  DateCalculations()
+        {
+            //Time Differnce
+
+            var timeSpan = new TimeSpan(60, 40, 4000);
+            var startDate = DateTimeOffset.UtcNow;
+            var endDate = startDate.AddMinutes(20).AddSeconds(24);
+
+            var differnce = endDate - startDate;
+
+            var calendar = CultureInfo.InvariantCulture.Calendar;
+            var startCalendar = DateTimeOffset.UtcNow;
+            var week = calendar.GetWeekOfYear(startCalendar.DateTime, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+
+            Console.WriteLine(week);
+
+
+
+            var isoWeek = ISOWeek.GetWeekOfYear(DateTimeOffset.UtcNow.DateTime);
+            Console.WriteLine(isoWeek);
+
             //Last Bit
 
             var dateTimeUTC = DateTimeOffset.UtcNow;
@@ -14,17 +44,17 @@ namespace DateTime
             Console.WriteLine(dateTimeUTC.ToLocalTime());
             var datenow = System.DateTime.Now.ToString();
             var dateUTC = System.DateTime.Parse(datenow, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal).ToUniversalTime();
-            var date =DateTimeOffset.Parse(datenow,CultureInfo.GetCultureInfo("en"));
+            var date = DateTimeOffset.Parse(datenow, CultureInfo.GetCultureInfo("en"));
             var date2 = System.DateTimeOffset.Now.ToString("dd MMMM yyyy", CultureInfo.GetCultureInfo("sv-SE"));
             var time = DateTimeOffset.Now;
-             
+
             Console.WriteLine(dateUTC);
 
             // Get Timezone 
 
-            foreach(var timezone in TimeZoneInfo.GetSystemTimeZones())
+            foreach (var timezone in TimeZoneInfo.GetSystemTimeZones())
             {
-                if(timezone.GetUtcOffset(time) == time.Offset)
+                if (timezone.GetUtcOffset(time) == time.Offset)
                 {
                     Console.WriteLine(timezone);
                 }
@@ -35,8 +65,8 @@ namespace DateTime
             var now = System.DateTime.Now;
             var timeZoneInfos = TimeZoneInfo.GetSystemTimeZones();
 
-            foreach(var timezone in timeZoneInfos)
-            {  
+            foreach (var timezone in timeZoneInfos)
+            {
                 var converted = TimeZoneInfo.ConvertTime(now, timezone);
                 Console.WriteLine($"{ timezone.Id}-{converted}");
             }
