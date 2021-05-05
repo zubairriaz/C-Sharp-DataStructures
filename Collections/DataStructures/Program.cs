f24c9c504e28be44b8ed377e600fe1d609dff3c0
Zubair <zubair.riaz@trapezegroup.com>
2021-05-06 04:38:04 +0500

    committing cahnges

--- Collections/DataStructures/Program.cs
+++ Collections/DataStructures/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DataStructures.Arrays;
 using DataStructures.SortingAlgos;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DataStructures
 {
@@ -13,10 +15,20 @@ namespace DataStructures
             int[] array = new[] { -11, 11, 2, -2, 9, 8, 7, 6 };
             var returnedarray = QuickSortAlgo.QuickSortAlgorithm(array);
 
-           foreach(var item in returnedarray)
+            var sortedList = new SortedList<int,string>();
+            sortedList.Add(1, "one");
+            sortedList.Add(2, "one");
+            sortedList.Add(3, "one");
+            sortedList.Add(4, "one");
+            sortedList.Add(5, "one");
+
+            var key = sortedList.ContainsKey(1);
+            foreach(var item in sortedList)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item.Value, key);
             }
+
+       
         }
     }
 }
