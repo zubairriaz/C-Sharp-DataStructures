072f2edceb6ae1cb69a2b435b47799e443e54f96
Zubair <zubair.riaz@trapezegroup.com>
2021-05-12 06:08:08 +0500

    Binary Search Trees Implemented

---
+++ Collections/DataStructures/BinarySearchTrees/BinarySearchTrees.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.BinarySearchTrees
+{
+    public class BinarySearchTree<T> : BinaryTree<T> where T: IComparable
+    {
+        public bool isContains (T data)
+        {
+            BinaryTreeNode<T> node = Root;
+            while (node != null)
+            {
+                int result = node.Data.CompareTo(data);
+                if (result == 0)
+                {
+                    return true;
+                }
+                else if(result < 0)
+                {
+                    node = node.LeftChild;
+                }
+                else if(result > 0)
+                {
+                    node = node.RightChild;
+                }
+             
+
+            }
+            return false;
+           
+        }
+
+        public void Add(T data)
+        {
+            BinaryTreeNode<T> Parent = GetParentForNewNode(data);
+            BinaryTreeNode<T> NewNode = new BinaryTreeNode<T>()
+            {
+                Data = data,
+                Parent = Parent
+            };
+
+            if (Parent == null)
+            {
+                Root = NewNode;
+            }
+            else if (NewNode.Data.CompareTo(Parent.Data) > 0)
+            {
+                Parent.RightChild = NewNode;
+            }
+            else if (NewNode.Data.CompareTo(Parent.Data) < 0)
+            {
+                Parent.LeftChild = NewNode;
+            }
+        }
+
+        private BinaryTreeNode<T> GetParentForNewNode(T data)
+        {
+            BinaryTreeNode<T> current = Root;
+            BinaryTreeNode<T> parent = null;
+            while (current != null)
+            {
+                parent = current;
+                if (data.CompareTo(current.Data) == 0)
+                {
+                    throw new ArgumentException("node Already exists");
+                }
+                else if (data.CompareTo(current.Data) > 0)
+                {
+                    current = current.RightChild;
+                }
+                else
+                {
+                    current = current.LeftChild;
+                }
+            }
+            return parent;
+        }
+    }
+}
---
+++ Collections/DataStructures/BinarySearchTrees/BinaryTress.cs
@@ -0,0 +1,94 @@
+﻿using DataStructures.Trees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.BinarySearchTrees
+{
+    public class BinaryTreeNode<T> :  TreeNode<T>
+    {
+        public BinaryTreeNode()
+        {
+            Children = new List<TreeNode<T>>() { null, null };
+        }
+
+        public BinaryTreeNode<T> LeftChild { get { return (BinaryTreeNode<T>)Children[0]; } set { Children[0] = value; } }
+        public BinaryTreeNode<T> RightChild { get { return (BinaryTreeNode<T>)Children[1]; } set { Children[1] = value; } }
+
+    }
+
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+        public int Count { get; set; }
+
+        private void TraversePreOrder(BinaryTreeNode<T> node , List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                result.Add(node);
+                TraversePreOrder(node.LeftChild, result);
+                TraversePreOrder(node.RightChild, result);
+            }
+        }
+
+        private void TraverseInOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                TraverseInOrder(node.LeftChild, result);
+                result.Add(node);
+                TraverseInOrder(node.RightChild, result);
+            }
+        }
+
+        private void TraversePostOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                TraversePostOrder(node.LeftChild, result);
+                TraversePostOrder(node.RightChild, result);
+                result.Add(node);
+
+            }
+        }
+
+        public List<BinaryTreeNode<T>> Traverse(TraversalEnum mode)
+        {
+            var result = new List<BinaryTreeNode<T>>();
+            switch (mode)
+            {
+                case TraversalEnum.INORDER:
+                    TraverseInOrder(Root, result);
+                    break;
+                case TraversalEnum.PREORDER:
+                    TraversePreOrder(Root, result);
+                    break;
+                case TraversalEnum.POSTORDER:
+                    TraversePostOrder(Root, result);
+                    break;
+
+
+            }
+
+            return result;
+        }
+
+        public int GetHeight()
+        {
+            int height = 0;
+            foreach(var item in Traverse(TraversalEnum.PREORDER))
+            {
+                height = Math.Max(height, item.GetHeight());
+            }
+            return height;
+        }
+    }
+
+    public enum TraversalEnum
+    {
+        PREORDER,
+        INORDER,
+        POSTORDER
+    }
+}
